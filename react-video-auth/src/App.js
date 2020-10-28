4536ad3ecd9fd90584aa28bea2efdec838f3d2f1
mamadou sylla <m27sylla@gmail.com>
2020-10-28 12:31:18 +0100

    test affchage login page bug

--- react-video-auth/src/App.js
+++ react-video-auth/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase from "./utils/firebaseConfig";
+import Main from "./components/Main";
 
 const App = () => {
-  const [isSignedIn, setSignedIn] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
   const uiConfig = {
     signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
@@ -18,12 +21,27 @@ const App = () => {
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
-      setSignedIn(!!user);
+      // !! ensure boolean
+      setIsSignedIn(!!user);
       console.log(user);
     });
-  },[]);
+  }, []);
 
-  return <div></div>;
+  return (
+    <div className="App" style={{ textAlign: "center" }}>
+      {isSignedIn ? (
+        <Main />
+      ) : (
+        <div className="login-page">
+          <h1>React Crud</h1>
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
---
+++ react-video-auth/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+	return (
+		<div>
+			Yo
+		</div>
+	);
+};
+
+export default Main;
